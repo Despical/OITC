7aefaf172c22193c92bea2bfb8232e07f774f0f7
Berke Akçen <berkeakcen@gmail.com>
2024-04-29 17:15:11 +0300

    Update license header

--- src/main/java/me/despical/oitc/handlers/setup/SetupUtilities.java
+++ src/main/java/me/despical/oitc/handlers/setup/SetupUtilities.java
@@ -1,6 +1,6 @@
 /*
  * OITC - Kill your opponents and reach 25 points to win!
- * Copyright (C) 2023 Despical
+ * Copyright (C) 2024 Despical
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
