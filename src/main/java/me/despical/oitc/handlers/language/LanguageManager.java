5a334263c9b65350879c7f5e0732d71958caf238
Berke Akçen <berkeakcen@gmail.com>
2024-07-23 14:15:56 +0300

    Added Russian locale

--- src/main/java/me/despical/oitc/handlers/language/LanguageManager.java
+++ src/main/java/me/despical/oitc/handlers/language/LanguageManager.java
@@ -40,8 +40,9 @@ public class LanguageManager {
 	private void registerLocales() {
 		Collections.listOf(
 				new Locale("English", "en_GB", "Despical", "default", "english", "en"),
-				new Locale("Turkish", "tr_TR", "Despical", "turkish", "türkçe", "turkce", "tr"),
-				new Locale("German", "de_DE", "Dreandor", "deutsch", "german", "de"),
+				new Locale("Turkish", "tr_TR", "Despical", "turkish", "türkçe", "turkce", "tr", "tr_TR"),
+				new Locale("German", "de_DE", "Dreandor", "deutsch", "german", "de", "de_DE"),
+				new Locale("Russian", "ru_RU", "WowExotic", "russian", "ru", "ru_RU"),
 				new Locale("Chinese (Taiwan)", "zh_TW", "HappyAreaBean", "zh_TW", "taiwan", "zh_TW"),
 				new Locale("Chinese (Hong Kong)", "zh_HK", "HappyAreaBean", "zh_HK", "honk_kong", "zh_HK"),
 				new Locale("Chinese (China)", "zh_CN", "HappyAreaBean", "zh_CN", "chinese", "zh_CN"))
